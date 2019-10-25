4245fd32f2c4ac0134471f04662db7654824f897
SirBruno <bruno.webjr@gmail.com>
2019-10-25 10:08:07 -0300

    ...

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ class Home extends React.Component {
       <div className="main-div">
         <div className="content"><h3><b>Lorem ipsum</b> dolor sit amet</h3></div>
         <div className="content"><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p></div>
-        <span><a class="waves-effect waves-light btn-large bg-orange-hvr">Button</a></span>
+        <span><a class="waves-effect waves-light btn-large bg-orange-hvr">btn</a></span>
       </div>
     )
   }
